76df67b5da73f1be1d67dfe54c10db5d23f4aaf7
zen <paulozenm@gmail.com>
2021-09-07 14:27:55 -0300

    feature: queries for data warehouse tables added

--- etl/sql_queries.py
+++ etl/sql_queries.py
@@ -11,7 +11,7 @@ cnaes_path = config['FILE_PATHS']['cnaes_path']
 
 
 
-# DROP TABLES 
+# DROP STAGING TABLES 
 drop_staging_table_cno_cnaes = '''
 DROP TABLE IF EXISTS cno_cnaes
 '''
@@ -21,12 +21,16 @@ DROP TABLE IF EXISTS cno_vinculos
 drop_staging_table_cno_obras = '''
 DROP TABLE IF EXISTS cno_obras
 '''
+
+# DROP DW TABLES
 drop_table_municipios = '''
 DROP TABLE IF EXISTS municipios
 '''
-
 drop_table_cnaes = '''
 DROP TABLE IF EXISTS cnaes
+'''
+drop_table_obras = '''
+DROP TABLE IF EXISTS obras
 '''
 
 # CREATE STAGING TABLES
@@ -46,7 +50,7 @@ CREATE TABLE IF NOT EXISTS staging_cno_vinculos(
     dt_inicio       DATE,
     dt_fim          DATE,
     dt_registro     DATE,
-    cod_quali       VARCHAR(10),
+    cod_quali       INT,
     ni_resp         VARCHAR(18)
 )
 '''
@@ -63,7 +67,7 @@ CREATE TABLE IF NOT EXISTS staging_cno_obras(
     cno_vinculado   VARCHAR(20),
     cep             VARCHAR(10),
     ni_resp         VARCHAR(18),
-    cod_qualiresp   VARCHAR(8),
+    cod_qualiresp   INT,
     nome_obra       VARCHAR(250),
     cod_mun_siafi   INT,
     nom_mun         VARCHAR(250),
@@ -76,7 +80,7 @@ CREATE TABLE IF NOT EXISTS staging_cno_obras(
     compl           VARCHAR(250),
     und_obra        VARCHAR(50),
     area_total      DECIMAL,
-    cod_sit         VARCHAR(5),
+    cod_sit         INT,
     dt_sit          DATE,
     resp_nome       VARCHAR(250)
 )
@@ -101,6 +105,42 @@ create_table_cnaes = '''
 CREATE TABLE IF NOT EXISTS cnaes(
     cod_cnae        INT           PRIMARY KEY,
     desc_cnae       VARCHAR(250)  NOT NULL    
+)
+'''
+
+# CREATE FACT TABLE
+create_fact_obras = '''CREATE TABLE IF NOT EXISTS obras(
+    obra_key        SERIAL        PRIMARY KEY,
+    cno             VARCHAR(15)   NOT NULL,
+    nom_pais        VARCHAR(50),
+    dt_ini_obra     DATE,
+    dt_ini_resp     DATE,
+    dt_reg          DATE,
+    cno_vinculado   VARCHAR(20),
+    cep             VARCHAR(10),
+    ni_resp         VARCHAR(18),
+    qualiresp       VARCHAR(250),
+    nome_obra       VARCHAR(250),
+    cod_mun_siafi   INT,
+    tipo_logr       VARCHAR(250),
+    logr            VARCHAR(250),
+    nro_logr        VARCHAR(10),
+    bairro          VARCHAR(250),
+    estado          VARCHAR(250),
+    cx_postal       VARCHAR(50),
+    compl           VARCHAR(250),
+    und_obra        VARCHAR(50),
+    area_total      DECIMAL,
+    sit_cad         VARCHAR(50),
+    dt_sit          DATE,
+    resp_nome       VARCHAR(250),
+    dt_ini_vinc     DATE,
+    dt_fim_vinc     DATE,
+    dt_reg_vinc     DATE,
+    quali_vinc      VARCHAR(250),
+    ni_resp_vinc    VARCHAR(250),
+    cnae            VARCHAR(10),
+    dt_reg_cnae     DATE
 )
 '''
 
@@ -146,6 +186,74 @@ CSV HEADER
 ENCODING 'latin1'
 '''
 
+# FACT TABLE DATA INGEST
+insert_data_fact_table = '''
+INSERT INTO obras (cno, nom_pais, dt_ini_obra, dt_ini_resp, dt_reg,       
+                    cno_vinculado, cep, ni_resp, qualiresp, nome_obra, cod_mun_siafi,
+                    tipo_logr, logr, nro_logr, bairro, estado, cx_postal, compl, und_obra, 
+                    area_total, sit_cad, dt_sit, resp_nome, dt_ini_vinc, dt_fim_vinc, 
+                    dt_reg_vinc, quali_vinc, ni_resp_vinc, cnae, dt_reg_cnae)
+
+SELECT o.cno            AS      cno,
+       o.nom_pais       AS      nom_pais,
+       o.dt_inicio      AS      dt_ini_obra,
+       o.dt_inicio_resp AS      dt_ini_resp,
+       o.dt_registro    AS      dt_reg,
+       o.cno_vinculado  AS      cno_vinculado,
+       o.cep            AS      cep,
+       o.ni_resp        AS      ni_resp,
+       CASE WHEN o.cod_qualiresp = 70  THEN 'Proprietário do Imóvel'
+            WHEN o.cod_qualiresp = 57  THEN 'Dono da Obra'
+            WHEN o.cod_qualiresp = 64  THEN 'Incorporadora de Construção Civil'
+            WHEN o.cod_qualiresp = 53  THEN 'Pessoa Jurídica Construtora'
+            WHEN o.cod_qualiresp = 111 THEN 'Sociedade Líder de Consórcio'
+            WHEN o.cod_qualiresp = 109 THEN 'Consórcio'
+            WHEN o.cod_qualiresp = 110 THEN 'Construção em nome coletivo'
+            ELSE 'Não Informado'
+            END         AS      qualiresp,
+       o.nome_obra      AS      nome_obra,
+       o.cod_mun_siafi  AS      cod_mun_siafi,
+       o.tipo_logr      AS      tipo_logr,
+       o.logr           AS      logr,
+       o.nro_logr       AS      nro_logr,
+       o.bairro         AS      bairro,
+       o.estado         AS      estado,
+       o.cx_postal      AS      cx_postal,
+       o.compl          AS      compl,
+       o.und_obra       AS      und_obra,
+       o.area_total     AS      area_total,
+       CASE WHEN o.cod_sit = 2  THEN 'Ativa'
+            WHEN o.cod_sit = 14 THEN 'Paralisada'
+            WHEN o.cod_sit = 15 THEN 'Encerrada'
+            WHEN o.cod_sit = 1  THEN 'Nula'   
+            WHEN o.cod_sit = 3  THEN 'Suspensa'
+            ELSE 'Não Informado'
+            END         AS      sit_cad,
+       o.dt_sit         AS      dt_sit,
+       o.resp_nome      AS      resp_nome,
+       v.dt_inicio      AS      dt_ini_vinc,
+       v.dt_fim         AS      dt_fim_vinc,
+       v.dt_registro    AS      dt_reg_vinc,
+       CASE WHEN v.cod_quali = 70  THEN 'Proprietário do Imóvel'
+            WHEN v.cod_quali = 57  THEN 'Dono da Obra'
+            WHEN v.cod_quali = 64  THEN 'Incorporadora de Construção' 
+            WHEN v.cod_quali = 53  THEN 'Pessoa Jurídica Construtora'
+            WHEN v.cod_quali = 111 THEN 'Sociedade Líder de Consórcio'
+            WHEN v.cod_quali = 109 THEN 'Consórcio'
+            WHEN v.cod_quali = 110 THEN 'Construção em nome coletivo'
+            ELSE 'Não Informado'
+            END         AS      quali_vinc,
+       v.ni_resp        AS      ni_resp_vinc,
+       c.cnae           AS      cnae,
+       c.dt_registro    AS      dt_reg_cnae
+FROM staging_cno_obras o
+LEFT JOIN staging_cno_vinculos v
+ON o.cno = v.cno
+LEFT JOIN staging_cno_cnaes c
+ON o.cno = c.cno
+'''
+
+
 # LIST OF STAGING QUERIES
 drop_staging_tables_queries = [drop_staging_table_cno_cnaes, drop_staging_table_cno_vinculos, 
                         drop_staging_table_cno_obras]
@@ -154,7 +262,7 @@ create_staging_tables_queries = [create_staging_table_cno_cnae, create_staging_t
 copy_staging_data_queries = [copy_staging_cno_cnaes, copy_staging_cno_vinculos, 
                             copy_staging_cno_obras]
                         
-# LIST OF DIMENSION QUERIES
-drop_dimension_tables_queries = [drop_table_municipios, drop_table_cnaes]
-create_dimension_tables_queries = [create_table_municipios, create_table_cnaes]
-coyp_staging_data_queries = [copy_municipios, copy_cnaes]
+# LIST OF DATA WAREHOUSE QUERIES
+drop_dw_tables_queries = [drop_table_municipios, drop_table_cnaes, drop_table_obras]
+create_dw_tables_queries = [create_table_municipios, create_table_cnaes, create_fact_obras]
+data_ingestion_queries = [copy_municipios, copy_cnaes, insert_data_fact_table, insert_data_fact_table]
